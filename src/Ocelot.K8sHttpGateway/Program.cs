d099863ab778e9ac72c199127c226d3bc01185e4
YangChao <yangch_ts@qq.com>
2019-02-12 15:47:20 +0800

    add consful

---
+++ src/Ocelot.K8sHttpGateway/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Ocelot.K8sHttpGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel((hostingContext, options) =>
+                {
+                    var maxBodySize = hostingContext.Configuration["MaxRequestSize"];
+                    if (!string.IsNullOrWhiteSpace(maxBodySize)
+                        && int.TryParse(maxBodySize, out var mrbs))
+                    {
+                        options.Limits.MaxRequestBodySize = mrbs * 1024L * 1024L;
+                    }
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile(Path.Combine("config", "appsettings.json"), false, true)
+                        .AddJsonFile(Path.Combine("config", $"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json"), true, true)
+                        .AddJsonFile(Path.Combine("config", "ocelot.json"), false, true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    //add logging
+                    logging.AddConsole();
+                    if (hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.SetMinimumLevel(LogLevel.Debug);
+                    }
+                    else
+                    {
+                        logging.SetMinimumLevel(LogLevel.Error);
+                    }
+                })
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ src/Ocelot.K8sHttpGateway/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+
+namespace Ocelot.K8sHttpGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            //解决Multipart body length limit 134217728 exceeded
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue;
+            });
+
+            services
+                .AddOcelot()
+                .AddConsul()
+                .AddConfigStoredInConsul();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder =>
+                builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
